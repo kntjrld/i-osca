0eb299858d737fc1adb618805e3273490a7a833a
kntjrld <68082685+kntjrld@users.noreply.github.com>
2022-12-07 21:31:05 +0800

    Added select option for active and inactive members

--- function/records_query.php
+++ function/records_query.php
@@ -1,16 +1,16 @@
 <?php
-
 $xx = $_SESSION['user_level'];
 $street = $_SESSION['fx_street'];
 if($_SESSION['user_level'] == 'staff'){
-    $records = $conn->query("SELECT * FROM tbl_records WHERE fx_barangay = '$street' AND account_status = 'active'");   
+    $records = $conn->query("SELECT * FROM tbl_records WHERE fx_barangay = '$street' AND account_status = 'active'");
+    $inactive = $conn->query("SELECT * FROM tbl_records WHERE fx_barangay = '$street' AND account_status = 'inactive'");   
 }else{
     $records = $conn->query("SELECT * FROM tbl_records WHERE account_status = 'active'");
+    $inactive = $conn->query("SELECT * FROM tbl_records WHERE account_status = 'inactive'");
 }
 
 function dt_format($date){
     $formatter = strtotime($date);
     return $format_date = date('m/d/Y', $formatter);
 }
-
 ?>
